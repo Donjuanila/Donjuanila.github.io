0b234c96b5c20709d714245cced40f8c1b7ade66
Alzbeta Dlha <alzbeta.dlha@gmail.com>
2016-03-03 20:16:27 +0000

    First ever git commit, thanks mum

---
+++ landingpage.js
@@ -0,0 +1,10 @@
+<SCRIPT LANGUAGE="JavaScript">
+
+$("#sketch").mousemove(function(e){
+  $('.follow').css({'top': e.clientY - 20, 'left': e.clientX - 20});
+});
+
+
+<SCRIPT LANGUAGE="JavaScript">
+and
+</SCRIPT>
